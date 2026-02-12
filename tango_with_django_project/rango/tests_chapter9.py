981847944ffbbc0cd907dcd66ff55b0a8da12318
“Lewis <3007573f@student.gla.ac.uk>
2026-02-12 18:29:01 +0000

    Chapter 9

--- tango_with_django_project/rango/tests_chapter9.py
+++ tango_with_django_project/rango/tests_chapter9.py
@@ -1,6 +1,22 @@
-from django.test import TestCase
+# 
+# Tango with Django 2 Progress Tests
+# By Leif Azzopardi and David Maxwell
+# With assistance from Gerardo A-C (https://github.com/gerac83) and Enzo Roiz (https://github.com/enzoroiz)
+# 
+# Chapter 9 -- Forms
+# Last updated: February 6th, 2020
+# Revising Author: David Maxwell
+# 
 
-# Create your tests here.
+#
+# In order to run these tests, copy this module to your tango_with_django_project/rango/ directory.
+# Once this is complete, run $ python manage.py test rango.tests_chapter9
+# 
+# The tests will then be run, and the output displayed -- do you pass them all?
+# 
+# Once you are done with the tests, delete the module. You don't need to put it in your Git repository!
+#
+
 import os
 import re
 import inspect
@@ -590,3 +606,5 @@ class Chapter9ExerciseTests(TestCase):
         user_object = create_user_object()
         self.client.login(username='testuser', password='testabc123')
         content = self.client.get(reverse('rango:show_category', kwargs={'category_name_slug': 'python'})).content.decode()
+
+        self.assertTrue(reverse('rango:add_page', kwargs={'category_name_slug': 'python'}) in content, f"{FAILURE_HEADER}The Add Page link was not present when a user was logged in, and looking at the show_category() view. Did you make a mistake in your category.html template?{FAILURE_FOOTER}")
